09d27ef5458c4b37ef4fa399937869755ce0075f
Kevin Contreras <kvncont@gmail.com>
2020-04-15 02:15:30 -0600

    App designs completada

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
  
 import 'package:designs/src/pages/basico_page.dart';
+import 'package:designs/src/pages/botones_page.dart';
 import 'package:designs/src/pages/scroll_page.dart';
 
 void main() => runApp(MyApp());
@@ -8,13 +10,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light
+    ));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basico': (BuildContext context) => BasicoPage(),
         'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage(),
       },
     );
   }
